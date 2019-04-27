a41ae4709640a1cc03d71bed628e79b89684ee9e
Quantum158 <lastisfirst54321@gmail.com>
2019-04-27 00:37:59 -0600

    Config.py and integration

---
+++ V2/config.py
@@ -0,0 +1,15 @@
+#TIMERS
+enabledTimers = dict(
+	TimerA = False,
+
+	TimerB = False
+	)
+
+#QUALITY OF LIFE
+delays = dict(
+	Delay_After_Staging = 0,
+
+	Start_Check_Delay = 2,
+
+	Post_Sequence_Cooldown = 3
+	)
