89beed87a03055aa4731fb621c9cd6b4cd26dca9
Quantum158 <lastisfirst54321@gmail.com>
2019-08-10 04:29:38 -0600

    Added App

---
+++ SpeedController/modules/config.py
@@ -0,0 +1,18 @@
+#THIS FILE IS CHANGED IN MEMORY (FOR NOW), THESE ARE THE DEFAULT VALUES (THE ONES FOR BOOT UP)
+
+
+#TIMERS
+enabledTimers = dict(
+	TimerA = False,
+
+	TimerB = False
+	)
+
+#QUALITY OF LIFE
+delays = dict(
+	Delay_After_Staging = 0, #In Seconds
+
+	PacerBeeps = 0, #Number of Beeps, 1 per Second
+	
+	Auto_Record_Stop = 0, #Number of seconds after recording started where camera should automatically shut off
+	)
